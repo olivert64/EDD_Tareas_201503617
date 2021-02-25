4b19a7cf031d94e2358c16a8610b0f5dbcf00ca5
olivert64 <herlindovargas@gmail.com>
2021-02-24 21:53:47 -0600

    prueba

--- Tarea3/main.go
+++ Tarea3/main.go
@@ -2,6 +2,41 @@ package main
 
 import "fmt"
 
+type nodo struct {
+	nombre, apellido, apodo, favoritos string
+	Siguiente, Anterior                *nodo
+}
+
+type lista struct {
+	cabeza *nodo
+	cola   *nodo
+}
+
+func (this *lista) Insertar(nuevo *nodo) {
+	if this.cabeza == nil {
+		this.cabeza = nuevo
+		this.cola = nuevo
+	} else {
+		this.cola.Siguiente = nuevo
+		nuevo.Anterior = this.cola
+		this.cola = nuevo
+	}
+}
+
 func main() {
-	fmt.Println("hola mundo")
+	li := lista{nil, nil}
+	a := nodo{"Marvin", "Martinez", "Marvin25ronal", "Jugar apex", nil, nil}
+	b := nodo{"Yaiza", "Pineda", "Bambi", "Patinar", nil, nil}
+	c := nodo{"Jonathan", "Lopez", "Pancho", "Comer", nil, nil}
+	d := nodo{"usuario1", "bla", "bla", "Jugar apex", nil, nil}
+	e := nodo{"usuario2", "bla", "bla", "Jugar apex", nil, nil}
+	f := nodo{"usuario3", "sale edd", "vamos con todo", "100 en la fase 1", nil, nil}
+	li.Insertar(&a)
+	li.Insertar(&b)
+	li.Insertar(&c)
+	li.Insertar(&d)
+	li.Insertar(&e)
+	li.Insertar(&f)
+
+	fmt.Println(li)
 }
